efabac890dffd7a28425167878c2f698a27bfe36
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-09-12 22:29:14 +0200

    first commit

---
+++ app/src/main/java/com/androideradev/www/moviespots/network/MovieDatabaseApi.java
@@ -0,0 +1,5 @@
+package com.androideradev.www.moviespots.network;
+
+interface MovieDatabaseApi {
+}
+
---
+++ app/src/main/java/com/androideradev/www/moviespots/network/MovieDatabaseApiService.java
@@ -0,0 +1,5 @@
+package com.androideradev.www.moviespots.network;
+
+class MovieDatabaseApiService {
+
+}
